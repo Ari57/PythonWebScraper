91e7625ade49d4b8872b12b5ec394faab3d533ce
Ari57 <njds7777@gmail.com>
2022-06-18 21:18:49 +0100

    Now retrieves job title, company and location. Next is to allow the user to searcch for a job

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(soup.title)
 
-results = soup.find_all("h2")
+    
+    
+jobs = soup.find_all("div", class_="card-content")
+for job in jobs:
+        title = job.find("h2", class_= "title is-5")
+        company = job.find("h3", class_="subtitle is-6 company")
+        location = job.find("p", class_="location")
+        print(title.text.strip())
+        print(company.text.strip())
+        print(location.text.strip())
+        print()
+     
 
-print(results)
 
 # class = "title is-5" - Job Title
 # class = "subtitle is-6 company" - Name of company
 # class = "location" - Location
+
+

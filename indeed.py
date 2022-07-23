126e1ee37515b386c258ebbeada7dc4f082520ed
Ari57 <njds7777@gmail.com>
2022-07-23 21:30:59 +0100

    Made changes to fix accessing multiple results

--- indeed.py
+++ indeed.py
@@ -2,11 +2,9 @@ import requests
 from openpyxl import Workbook
 from openpyxl.styles import Font
 from bs4 import BeautifulSoup
-import os
 import time
 
 start_time = time.time()
-
 
 wb = Workbook()
 ws=wb.active
@@ -18,42 +16,47 @@ linkArray = []
 salaryArray = []
 
 
-def page(TitleSearch, LocationSearch):
-   for start in range(20):
-        URL = "https://uk.indeed.com/jobs?q="+TitleSearch+"%20&l="+LocationSearch+"%20&start={}".format(start * 10)
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, "html.parser")
-        jobs = soup.find_all("td", class_="resultContent")
-        return jobs
+def page(pageNo, TitleSearch, LocationSearch):
+    URL = "https://uk.indeed.com/jobs?q="+TitleSearch+"%20&l="+LocationSearch+"%20&start={}".format(pageNo)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    jobs = soup.find_all("td", class_="resultContent")
+    return jobs
     
 
 def results(jobs, jobWord):
     mainURL = "https://uk.indeed.com"
     
-    for job in jobs:
+    for job in jobs: 
         try:
             title = job.find("a", class_="jcs-JobTitle").text
+        except:
+            title = "No title found"
+        try:
             company = job.find("span", class_="companyName").text
+        except:
+            company = "No company found"
+        try:
             location = job.find("div", class_="companyLocation").text
+        except:
+            location = "No location found"
+        try:
             salary = job.find("div", class_="metadata salary-snippet-container").text
+        except:
+            salary = "No salary found"
+        try:         
             link = job.find("a").get("href")
-        
-            if title.find(jobWord) or title.find(jobWord.lower()) != -1:
+        except:
+            link = "No link found"
+            
+        if title.find(jobWord) or title.find(jobWord.lower()) != -1:
                 titleArray.append(title)
                 companyArray.append(company)
                 locationArray.append(location)
                 salaryArray.append(salary)
                 linkArray.append(mainURL + link)
-        except:
-                pass
-    return titleArray, companyArray, locationArray, salaryArray, linkArray
-            
-            
-def publish(titleArray, companyArray, locationArray, salaryArray, linkArray):
-    i = 2
-
-    length = len(titleArray)
-    
+       
+                
     ws.cell(row=1, column=1, value="Title")
     ws.cell(row=1, column=2, value="Company")
     ws.cell(row=1, column=3, value="Location")
@@ -65,8 +68,12 @@ def publish(titleArray, companyArray, locationArray, salaryArray, linkArray):
     ws["C1"].font = Font(bold=True)
     ws["D1"].font = Font(bold=True)
     ws["E1"].font = Font(bold=True)
+    
+    length = len(titleArray)
+    
+    i = 2
 
-    for j in range(0,length):
+    for j in range(0,length): 
         ws.cell(row=i, column=1, value=titleArray[j])
         ws.cell(row=i, column=2, value=companyArray[j])
         ws.cell(row=i, column=3, value=locationArray[j])
@@ -75,17 +82,18 @@ def publish(titleArray, companyArray, locationArray, salaryArray, linkArray):
         i+=1
         
     wb.save("jobs.xlsx")
-    print("File is located at " + os.path.abspath("jobs.xlsx"))
+        
+    
     
 def main():
-    jobs = page("Software Developer", "London")
-    results(jobs, "Developer")
+    for page_order in range(0, 40, 10):
+        extract = page(page_order, "Software Developer", "London")
+        publish = results(extract, "Developer")
     
-    titleArray, companyArray, locationArray, salaryArray, linkArray = results("", "")
-    publish(titleArray, companyArray, locationArray, salaryArray, linkArray)
     
     # https://uk.indeed.com/jobs?q=Software%20Developer&l=London&start=10
 
 main()
+
 
 print(time.time() - start_time)
